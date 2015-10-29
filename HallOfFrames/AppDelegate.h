86b3d52d72c1b1baf0cc39bc875ea45f6cb545a5
Martin Henry <emailnitram@gmail.com>
2015-10-29 15:06:46 -0700

    Initial Commit

---
+++ HallOfFrames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrames
+//
+//  Created by Martin Henry on 10/29/15.
+//  Copyright © 2015 Martin Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HallOfFrames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HallOfFrames
+//
+//  Created by Martin Henry on 10/29/15.
+//  Copyright © 2015 Martin Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
